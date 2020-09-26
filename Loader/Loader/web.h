87ca68f2a5a18110ea5493f18920ddbc14afcfca
Xoria Niveres <x.niveres@gmail.com>
2020-09-26 14:06:21 +0200

    Added Loader and updated web files

---
+++ Loader/Loader/web.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "inc.h"
+namespace web
+{
+	typedef enum
+	{
+		GET,
+		POST
+	} requestmode;
+	class client
+	{
+		int size;
+		int stage;
+		bool      usehttps;
+		bool      insecure;
+		HINTERNET hsession = nullptr;
+		HINTERNET hconnect = nullptr;
+		HINTERNET hrequest = nullptr;
+	private:
+		std::vector<byte> response;
+		std::string url_encode(const std::string& value);
+	public:
+		bool               connect(std::wstring page);
+		bool               request(std::wstring file, web::requestmode mode, std::unordered_map<std::string, std::string> vars);
+		std::vector<byte>  get();
+		void			   set_max(int s, int st);
+		static std::string tostring(std::vector<byte> in);
+	public:
+		client(bool usehttps = true, bool insecure = false);
+		~client();
+	};
+}
